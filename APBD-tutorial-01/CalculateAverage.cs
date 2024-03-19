55f0dd1c21c1b3426311f8e7e40617f8f78e6b15
s23985 <s23985@pjwstk.edu.pl>
2024-03-19 20:08:25 +0100

    Modification 4

---
+++ APBD-tutorial-01/CalculateAverage.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD_tutorial_01;
+
+public class CalculateAverage
+{
+    public static decimal Calculate(int[] array)
+    {
+        var sum = (decimal)array.Sum(x => x);
+        return sum / array.Length;
+    }
+}
--- APBD-tutorial-01/Program.cs
+++ APBD-tutorial-01/Program.cs
@@ -6,9 +6,13 @@ public class Program
 {
     public static void Main()
     {
-        for (var i = 0; i < 10; i++)
+        for (var i = 0; i < 3; i++)
         {
             Console.WriteLine("Hello, World!");
         }
+
+        var arr = new[] {1, 2, 3, 4, 5, 6};
+        
+        Console.WriteLine(CalculateAverage.Calculate(arr));
     }
 }
