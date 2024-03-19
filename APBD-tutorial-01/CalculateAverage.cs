6b3f18b776e43f74bbfbf7811e05de358c13b36d
s23985 <s23985@pjwstk.edu.pl>
2024-03-19 20:23:49 +0100

    Change logic in CalculateAverage

--- APBD-tutorial-01/CalculateAverage.cs
+++ APBD-tutorial-01/CalculateAverage.cs
@@ -4,7 +4,12 @@ public class CalculateAverage
 {
     public static decimal Calculate(int[] array)
     {
-        var sum = (decimal)array.Sum(x => x);
+        decimal sum = 0;
+        foreach (var i in array)
+        {
+            sum += i;
+        }
+
         return sum / array.Length;
     }
 }
